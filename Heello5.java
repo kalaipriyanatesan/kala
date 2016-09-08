896db64205c10350528af4da78fc3f0ede08854f
kalaipriyanatesan <kalaipriya.natesan@gmail.com>
2016-09-08 15:07:52 +0530

    Create Heello5.java

---
+++ Heello5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Heello5 {
+
+	
+	public static void main(String[] args) {
+		int a=5;
+		for(int i=1;i<=a;i++)
+		{
+			System.out.println("hello");
+		}
+	}
+
+}
