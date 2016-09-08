7ba65ceaa4f5998925b10222f6f64d0e55d1d227
kalaipriyanatesan <kalaipriya.natesan@gmail.com>
2016-09-08 11:49:18 +0530

    Create Pos.java

---
+++ Pos.java
@@ -0,0 +1,16 @@
+public class Pos {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		if (a == 0) {
+			System.out.println("zero");
+		} else if (a > 0) {
+			System.out.println("positive");
+		} else {
+			System.out.println("negztive");
+		}
+
+	}
+
+}
