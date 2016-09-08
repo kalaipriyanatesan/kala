8c1f994513e5fee9ed6a1b4330572b80c8b314b5
kalaipriyanatesan <kalaipriya.natesan@gmail.com>
2016-09-08 15:13:01 +0530

    Create Naturalnum.java

---
+++ Naturalnum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Naturalnum {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int r = 0;
+		for (int i = 1; i <= n; i++) {
+			r = n * (n - 1) / 2;
+		}
+		System.out.println(r);
+	}
+
+}
