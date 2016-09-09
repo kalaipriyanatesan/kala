14a1328299e79ee5871fd2497e0052cb9387428d
kalaipriyanatesan <kalaipriya.natesan@gmail.com>
2016-09-09 12:07:51 +0530

    Create Stringswap.java

---
+++ Stringswap.java
@@ -0,0 +1,21 @@
+
+class Stringswap
+{
+public static void main(string []args)
+{
+string input = "AXBYCZ";
+StringBuilder output = new StringBuilder();
+ 
+char[] characters = input.ToCharArray();
+ 
+for (int i = 0; i < characters.Length; i++)
+{
+  if (i % 2 == 0)
+  {
+    if((i+1) < characters.Length )
+    {
+      output.Append(characters[i + 1]);
+    }
+               output.Append(characters[i]);
+  }
+}
